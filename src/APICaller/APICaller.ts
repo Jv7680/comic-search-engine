2d439faff1c20bde397bb6d0d55b44a8167dae18
Trung Tin <justinvu768@gmail.com~>
2023-06-03 22:02:55 +0700

    change base url

--- src/APICaller/APICaller.ts
+++ src/APICaller/APICaller.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL: string = "http://localhost:3001";
+// const BASE_URL: string = "http://localhost:3001";
+const BASE_URL: string = "https://comic-search-engine-server.vercel.app";
 
 export const APICaller = axios.create({
     baseURL: BASE_URL,
